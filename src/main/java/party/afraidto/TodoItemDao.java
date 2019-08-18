6c7fa94c653d43a45d70cb24d369f22f32c8de72
maniacalhonks <maniacalhonks@gmail.com>
2019-08-18 00:16:39 -0400

    Remove old, commented-out TodoItemDao

--- src/main/java/party/afraidto/TodoItemDao.java
+++ src/main/java/party/afraidto/TodoItemDao.java
@@ -5,32 +5,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Component;
 import party.afraidto.domain.TodoItem;
 
-/*
-import org.springframework.beans.factory.annotation.Autowired;
-
-@Component
-public class TodoItemDao {
-    @Autowired
-    TodoItemMapper mapper;
-
-    public int insertItem(TodoItem item) {
-        System.out.println("insertItem -- about to call mapper");
-        TodoItem newItem = mapper.insertItem(item);
-        System.out.println("insertItem, after mapper, newItem = ");
-        System.out.println(newItem);
-        return newItem.getId();
-    }
-
-    public TodoItem getItem(int id) {
-        return mapper.getItem(id);
-    }
-
-    public List<TodoItem> getItems() {
-        return mapper.getItems();
-    }
-}
-*/
-
 @Component
 public class TodoItemDao {
 
